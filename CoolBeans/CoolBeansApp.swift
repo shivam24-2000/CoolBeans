dcefdbea851f1797f8bab00b790839b0560a530f
shivam24-2000 <shivamsinghal24@gmail.com>
2022-11-04 20:39:29 +0530

    app init

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Shivam Singhal on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
